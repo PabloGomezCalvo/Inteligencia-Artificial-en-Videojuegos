a6cf16071055c4debd83af5faf204b0966e26d6c
Pablo Gómez Calvo <pabgom03@ucm.es>
2020-02-27 00:08:11 +0100

    Botoncicos para la memoria

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botones : MonoBehaviour
+{
+  
+    public void Salir()
+    {
+        Application.Quit();
+    }
+    
+    public void GenerarMapa()
+    {
+        Debug.Log("MAPA NUEVO");
+    }
+
+}
