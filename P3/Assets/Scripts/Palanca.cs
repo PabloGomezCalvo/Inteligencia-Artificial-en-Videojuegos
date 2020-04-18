79a83e34d8423d645f5f334b7e979dfad101ae29
franlbc97 <franlbc97@gmail.com>
2020-04-18 20:40:36 +0200

    palanca empezada

---
+++ P3/Assets/Scripts/Palanca.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Palanca : MonoBehaviour
+{
+
+    public Material MatEnabled;
+    public Material MatDisabled;
+
+    [System.Serializable]
+    public class OnPalancaToggle : UnityEngine.Events.UnityEvent<bool> {};
+
+    [SerializeField]
+    public OnPalancaToggle OnInteract;
+
+
+    private MeshRenderer _mesh;
+
+    private bool _enabled;
+
+    public void Toggle()
+    {
+        _enabled = !_enabled;
+
+        OnInteract.Invoke(_enabled);
+        
+
+        if (_enabled)
+            _mesh.material = MatEnabled;
+        else
+            _mesh.material = MatDisabled;
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enabled = false;
+        _mesh = GetComponent<MeshRenderer>();
+        if(_mesh != null)
+        { 
+            _mesh.material = MatDisabled;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            Toggle();
+    }
+
+
+}
