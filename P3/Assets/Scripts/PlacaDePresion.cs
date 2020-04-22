3691ab64c1e359861a2df3b093702d7df01fdf85
Sergio j. Higuera Velasco <chequilin3@gmail.com>
2020-04-22 11:41:49 +0200

    placas de presion hechas

---
+++ P3/Assets/Scripts/PlacaDePresion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacaDePresion : MonoBehaviour
+{
+    public GameObject Puerta;
+    private IEnumerator coroutine;
+    public float TimeToReopen = 2.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Desactivo la puerta para poder pasar
+        Puerta.transform.Rotate(new Vector3(0,1,0),90);
+        //empiezo corutine para volver a cerrar
+        coroutine = Cerrar(TimeToReopen);
+        StartCoroutine(coroutine);
+    }
+
+
+    private IEnumerator Cerrar(float waitTime)
+    {
+        //espero el tiempo dado antes de volver a activar la puerta
+        yield return new WaitForSeconds(waitTime);
+        Puerta.transform.Rotate(new Vector3(0, 1, 0), -90);
+    }
+
+}
